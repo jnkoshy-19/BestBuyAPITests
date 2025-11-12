cabb0b288e5be6164a17e5b3fd075794b1417490
jishakoshy25 <jishakoshy25@yahoo.com>
2025-11-12 08:50:53 -0500

    added tests

--- src/test/java/bestbuyApiPlayground/tests/ProductsAPITests.java
+++ src/test/java/bestbuyApiPlayground/tests/ProductsAPITests.java
@@ -1,6 +1,8 @@
 package bestbuyApiPlayground.tests;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.Reporter;
 import org.testng.annotations.BeforeClass;
@@ -11,7 +13,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 public class ProductsAPITests {
-	
+
 	FileUtilities fileUtilities;
 
 	@BeforeClass
@@ -51,6 +53,76 @@ public class ProductsAPITests {
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
+
+	}
+
+	@Test
+	public void verifyPostProductsWithPayloadAsObject() {
+		Reporter.log("Executing verifyPostProductsWithPayloadAsObject..", true);
+		Map<String, Object> requestPayload = new HashMap<String, Object>();
+		requestPayload.put("name", "iPhone 17");
+		requestPayload.put("type", "mobile");
+		requestPayload.put("price", 800);
+		requestPayload.put("shipping", 30);
+		requestPayload.put("upc", "mobile");
+		requestPayload.put("description", "dual camera iphone");
+		requestPayload.put("manufacturer", "Apple");
+		requestPayload.put("model", "iPhone 17");
+
+		RestAssured.given().when().contentType(ContentType.JSON).body(requestPayload).post("/products").then()
+				.statusCode(201).log().all();
+
+	}
+
+	@Test
+	public void verifyPutProductsWithPayloadAsObject() {
+		Reporter.log("Executing verifyPutProductsWithPayloadAsObject..", true);
+		Map<String, Object> requestPayload = new HashMap<String, Object>();
+		requestPayload.put("name", "iPhone 17");
+		requestPayload.put("type", "mobile");
+		requestPayload.put("price", 800);
+		requestPayload.put("shipping", 30);
+		requestPayload.put("upc", "mobile");
+		requestPayload.put("description", "dual camera iphone");
+		requestPayload.put("manufacturer", "Apple");
+		requestPayload.put("model", "iPhone 17");
+
+		int id = RestAssured.given().when().contentType(ContentType.JSON).body(requestPayload).post("/products").then().log().all()
+				.extract().path("id");
+
+		Map<String, Object> requestPayload1 = new HashMap<String, Object>();
+		requestPayload1.put("name", "iPhone 17 Pro");
+		requestPayload1.put("type", "mobile");
+		requestPayload1.put("price", 800);
+		requestPayload1.put("shipping", 30);
+		requestPayload1.put("upc", "mobile");
+		requestPayload1.put("description", "dual camera iphone");
+		requestPayload1.put("manufacturer", "Apple");
+		requestPayload1.put("model", "iPhone 17 Pro");
+
+		RestAssured.given().when().contentType(ContentType.JSON).body(requestPayload1).put("/products/" + id).then()
+				.statusCode(200).log().all();
+
+	}
+	@Test
+	public void verifyDeleteProduct() {
+		Reporter.log("Executing verifyDeleteProduct..", true);
+		Map<String, Object> requestPayload = new HashMap<String, Object>();
+		requestPayload.put("name", "iPhone 17");
+		requestPayload.put("type", "mobile");
+		requestPayload.put("price", 800);
+		requestPayload.put("shipping", 30);
+		requestPayload.put("upc", "mobile");
+		requestPayload.put("description", "dual camera iphone");
+		requestPayload.put("manufacturer", "Apple");
+		requestPayload.put("model", "iPhone 17");
+
+		int id = RestAssured.given().when().contentType(ContentType.JSON).body(requestPayload).post("/products").then().log().all()
+				.extract().path("id");
+
+
+		RestAssured.given().when().delete("/products/" + id).then().statusCode(200).log().all();
+		RestAssured.given().when().get("/products/" + id).then().statusCode(404).log().body();
 
 	}
 }
